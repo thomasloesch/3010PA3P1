3b62aa862df1454d66d2e16aed24a5a87d78f01a
thomasloesch <tommy.loesch@gmail.com>
2015-03-22 16:24:49 -0400

    Made changes to the Connect4Game class

--- Connect4Game.java
+++ Connect4Game.java
@@ -7,25 +7,53 @@ import java.util.ArrayList;
 public class Connect4Game {
 	private int gameState;
 	private Column[] gameArray;
-	// TODO: add variable to hold the state of the game pieces
-	// IDEA: do so by making an array of subclasses (Column)
 	
-	public void Connect4Game() { gameState = 1; }
+	public final int CHECKER_PLAYER_1 = 1;
+	public final int CHECKER_PLAYER_2 = 2;
 	
-	public boolean makeMove() { return false; }
+	public final int ERROR = 0;
+	public final int PLAYER_1_TURN = 1;
+	public final int PLAYER_2_TURN = 2;
+	public final int PLAYER_1_WIN = 3;
+	public final int PLAYER_2_WIN = 4;
+	public final int TIE = 5;
+	
+	public void Connect4Game() { 
+		gameState = 1; 
+		gameArray = new Column[8];
+		for(int i = 0; i < gameArray.length; i++) gameArray[i] = new Column();
+	}
+	
+	public boolean makeMove(int col) { return gameArray[col].add(gameState); }
 	
 	public int getGameState() { return gameState; }
 	
-	public void reset() { }
+	public void reset() { for(int i = 0; i < gameArray.length; i++) gameArray[i].reset(); }
 	
-	public int getValueInLoc(int row, int col) { return 0; }
+	public int getValueInLoc(int row, int col) { return gameArray[col].getLoc(row); }
 	
 	public ArrayList<Integer> getAllPossibleMoves() {
 		return null; 
 	}
 	
 	private class Column {
+		private int top;
+		private int[] contents;
 		
-		public Column() { }
+		public Column() { contents = new int[6]; top = 0; }
+		
+		public boolean add(int x) { 
+			if(isFull() == true) return false;
+			contents[top++] = x; 
+			return true;
+		}
+		
+		public int getTop() { return top; }
+		
+		public int getLoc(int x) { return contents[x]; }
+		
+		public void reset() { for(int i = 0; i < contents.length; i++) contents[i] = 0; top = 0; }
+		
+		public boolean isFull() { if(top == 7) return true; else return false; }
 	}
 }
