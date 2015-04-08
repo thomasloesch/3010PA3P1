e859f25516b6921796cc9c140ca1dfb030db32ea
thomasloesch <tommy.loesch@gmail.com>
2015-04-07 20:02:17 -0400

    Finished code

--- Connect4Driver.java
+++ Connect4Driver.java
@@ -11,34 +11,64 @@ public class Connect4Driver {
 		game = new Connect4Game();
 		Scanner scan = new Scanner(System.in);
 		String input;
+		int move;
 		
 		do {
-			input = getInput(scan);
-			updateGame(input);
-			displayGame();
-		} while(game.getGameState() == Connect4Game.PLAYER_1_TURN || game.getGameState() == Connect4Game.PLAYER_2_TURN);
-		
-		// TODO: handle end of game and results display
-		// IDEA: make another do-while loop so that game can be played multiple times
+			displayGame(game);
+			do {
+				move = getInput(scan);
+				game.makeMove(move);
+				displayGame(game);
+			} while(game.getGameState() == Connect4Game.PLAYER_1_TURN || game.getGameState() == Connect4Game.PLAYER_2_TURN);
+			
+			System.out.print("Would you like to play another game? (yes/no) ");
+			input = scan.next();
+			System.out.println();
+			
+			if(input.equals("yes"))
+				game.reset();
+			
+		} while(input.equals("yes"));
 		
 		scan.close();
 		
 		System.exit(0);
 	}
 	
-	public static void updateGame(String input) {
+	public static void displayGame(Connect4Game game) {
+		System.out.print("====");
+		if(game.getGameState() == Connect4Game.PLAYER_1_TURN || 
+		   game.getGameState() == Connect4Game.PLAYER_2_TURN)		System.out.print("TURN " + game.getTurn());
+		else if(game.getGameState() == Connect4Game.PLAYER_1_WIN)	System.out.print("O WINS");
+		else if(game.getGameState() == Connect4Game.PLAYER_2_WIN)	System.out.print("X WINS");
+		else if(game.getGameState() == Connect4Game.TIE)			System.out.print("TIE");
+		else														System.out.print("ERROR");
+		System.out.println("====");
 		
-		
-	}
-	
-	public static void displayGame() {
-		
-		
+		for(int i = 5; i >= 0; i--)
+			displayRow(i, game);
+		System.out.println("--------");
+		if	(game.getGameState() == Connect4Game.PLAYER_1_TURN) System.out.print("O");
+		else													System.out.print("X");
+		System.out.println("'s turn.");
 	}
 
-	public static String getInput(Scanner scan) {
+	public static void displayRow(int row, Connect4Game game) {
+		for(int i = 0; i < 8; i++) {
+			if		(game.getValueInLoc(i, row) == Connect4Game.CHECKER_PLAYER_1) 	System.out.print("O");
+			else if (game.getValueInLoc(i, row) == Connect4Game.CHECKER_PLAYER_2) 	System.out.print("X");
+			else																	System.out.print(" ");
+		}
+		System.out.println();
+	}
+
+	public static int getInput(Scanner scan) {
+		int retval;
 		
+		System.out.print("Which column would you like to place a checker in? ");
+		retval = scan.nextInt();
 		
-		return "Input";
+		return retval;
 	}
 }
+
